81a264c66183b5b497975135287646f354cfcbf6
vu dang <vu.dang@sotatek.com>
2021-04-06 14:37:46 +0700

    update version test detail

--- src/domain/containers/VersionTest/VersionDetail/VersionDetail.js
+++ src/domain/containers/VersionTest/VersionDetail/VersionDetail.js
@@ -1,8 +1,43 @@
+import { useQuery } from 'hooks/axios.hooks';
 import React from 'react';
-// import data from '../mockVersionTest.json';
+import { Button, Container } from 'react-bootstrap';
+import Wrapper from './VersionDetail.style';
+import questionsMock from '../../Home/Detail/questionsMock.json';
+import Loading from 'components/Loading';
+import { format } from 'date-fns';
 
-const VersionDetail = () => {
-  return <div>xxx</div>;
+const VersionDetail = ({ id }) => {
+  const { data, loading } = useQuery({ url: `/admin/tests/${id}` });
+  console.log(data);
+
+  return (
+    <Wrapper>
+      {loading && <Loading />}
+      {!loading && data && (
+        <Container>
+          <div className="information">
+            <div>
+              <p className="col"> Tên đợt khảo sát: {data.data?.name || '-'} </p>
+              <p className="col">Mô tả đợt khảo sát: {data.data?.code || '-'}</p>
+              <p className="col">Mã code vào khảo sát: {data.data?.entryCode || ''}</p>
+              <p className="col">Trạng thái đợt khảo sát:{data.data?.isClose ? 'Đóng' : 'Mở'}</p>
+              <p className="col">Thời gian:{data.data?.timer + 'p'} </p>
+            </div>
+          </div>
+        </Container>
+      )}
+    </Wrapper>
+  );
 };
 
 export default VersionDetail;
+
+// code: "khaosatlan1"
+// description: "Đợt khảo sát lần 1"
+// entryCode: ""
+// id: 1
+// isClose: false
+// name: "Khảo sát lần 1"
+// questions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+// testVersionId: "qwerty"
+// timer: 90
---
+++ src/domain/containers/VersionTest/VersionDetail/VersionDetail.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: #f0ede9;
+  overflow: auto;
+
+  .information {
+    padding: 30px;
+    background-color: #fff;
+    margin: 10px 15px;
+    border-radius: 7px;
+  }
+`;
+
+export default Wrapper;
