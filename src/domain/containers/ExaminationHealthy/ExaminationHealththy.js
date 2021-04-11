0f9b96fb2fb5da57848bc0460771ef71e14f3d3e
zenon191 <ngocnghia19011999@gmail.com>
2021-04-11 22:14:51 +0700

    footer for examination

--- src/domain/containers/ExaminationHealthy/ExaminationHealththy.js
+++ src/domain/containers/ExaminationHealthy/ExaminationHealththy.js
@@ -82,6 +82,7 @@ const ExaminationHealth = () => {
           </Col>
         </Row>
       </Container>
+      <Footer />
     </Wrapper>
   );
 };
--- src/domain/containers/ExaminationHealthy/ExaminationHealthy.styles.js
+++ src/domain/containers/ExaminationHealthy/ExaminationHealthy.styles.js
@@ -3,15 +3,12 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   background-color: #f0ede9;
   min-height: 100vh;
-  padding: 40px 0;
+  padding: 40px 0 0;
 `;
 export default Wrapper;
 
 export const WrapperInfor = styled.div`
-  .position-footer {
-    /* position: fixed;
-    bottom: 0;
-    margin: auto; */
-    margin-top: 0;
+  .container {
+    min-height: calc(100vh - 210px);
   }
 `;
