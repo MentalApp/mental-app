c1aa1b7de2efa597ff21f5afc2e5080c5adb58f1
zenon191 <ngocnghia19011999@gmail.com>
2021-04-19 23:43:11 +0700

    update form anwers questions

--- src/domain/components/QuestionForm/QuestionForm.js
+++ src/domain/components/QuestionForm/QuestionForm.js
@@ -18,36 +18,68 @@ const QuestionForm = ({ question, index, resultTest, setResultTest }) => {
         <p className="format--quest">
           {index + 1}. {question?.question}
         </p>
-        <div className="anwer-wrapper">
-          <div>
-            <input
-              type="radio"
-              id="true"
-              name="question"
-              className="input-yes"
-              checked={resultTest[index]?.answer === 'Có'}
-              onChange={(e) => {
-                if (e.currentTarget.checked) {
-                  handleSelect({ test_pool_id: question?.id, answer: 'Có' });
-                }
-              }}
-            />
-            <label htmlFor="true">Có</label>
-          </div>
-          <div>
+        <div className="row">
+          <div className="col-md-6 col-sm-12">
             <input
               type="radio"
               id="false"
               name="question"
-              className="input-no"
-              checked={resultTest[index]?.answer === 'Không'}
+              className="input-yes"
+              checked={resultTest[index]?.answer === 'Không có'}
               onChange={(e) => {
                 if (e.currentTarget.checked) {
-                  handleSelect({ test_pool_id: question?.id, answer: 'Không' });
+                  handleSelect({ test_pool_id: question?.id, answer: 'Không có' });
                 }
               }}
             />
-            <label htmlFor="false">Không</label>
+            <label htmlFor="false">Không có</label>
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="radio"
+              id="sometime"
+              name="question"
+              className="input-no"
+              checked={resultTest[index]?.answer === 'Thỉnh thoảng'}
+              onChange={(e) => {
+                if (e.currentTarget.checked) {
+                  handleSelect({ test_pool_id: question?.id, answer: 'Thỉnh thoảng' });
+                }
+              }}
+            />
+            <label htmlFor="sometime">Thỉnh thoảng</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="radio"
+              id="usually"
+              name="question"
+              className="input-no"
+              checked={resultTest[index]?.answer === 'Thường xuyên'}
+              onChange={(e) => {
+                if (e.currentTarget.checked) {
+                  handleSelect({ test_pool_id: question?.id, answer: 'Thường xuyên' });
+                }
+              }}
+            />
+            <label htmlFor="usually">Thường xuyên</label>
+          </div>
+          <div className="col-md-6 col-sm-12">
+            <input
+              type="radio"
+              id="always"
+              name="question"
+              className="input-no"
+              checked={resultTest[index]?.answer === 'Hầu như cả ngày'}
+              onChange={(e) => {
+                if (e.currentTarget.checked) {
+                  handleSelect({ test_pool_id: question?.id, answer: 'Hầu như cả ngày' });
+                }
+              }}
+            />
+            <label htmlFor="always">Hầu như cả ngày</label>
           </div>
         </div>
       </div>
